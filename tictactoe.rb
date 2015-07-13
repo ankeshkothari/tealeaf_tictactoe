f5b78723dcf3cb797688715bb6bf599b694139f4
Ankesh Kothari <ankeshk@ankeshkothari.com>
2015-07-13 13:26:19 +0530

    fixed winning_lines error

--- tictactoe.rb
+++ tictactoe.rb
@@ -38,7 +38,7 @@ end
 # Check for a win
 # If 3 squares in a row has same value, then win == true
 def check_win(b)
-  winning_lines = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[3,5,8],[3,6,9],[1,5,9],[3,5,7]]
+  winning_lines = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
   winning_lines.each do |x,y,z|
     if b[x] == b[y] && b[x] == b[z] && b[x] == "X"
       return "You"
